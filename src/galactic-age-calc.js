315dcd1e3dabf2e11498273d81c93f9e7bcb98e9
Student <example@example.com>
2020-02-07 13:26:46 -0800

    continue to troubleshoot galactic age calculator

--- src/galactic-age-calc.js
+++ src/galactic-age-calc.js
@@ -1,6 +1,8 @@
 export class GalacticAge	{
 	constructor(age)  {
 		this.age = age;
+		this.planetSolarYears = [87.6, 224.548, 365, 686.5285, 4329.557, 10759, 30688.5, 60182];
+		this.galacticAges = [];
 	}
 	
 	solarAge(planetSolarYear)	{
@@ -9,12 +11,10 @@ export class GalacticAge	{
 	}
 	
 	calculateGalacticAges() {
-		const planetSolarYears = [87.6, 224.548, 365, 686.5285, 4329.557, 10759, 30688.5, 60182];
-		this.galacticAges = [];
-		planetSolarYears.forEach(function(planetSolarYear) {
-			let planetSolarAge = solarAge(planetSolarYear);
-			this.galacticAges.push(planetSolarAge);
-			return this.galacticAges;			
+		this.planetSolarYears.forEach(function(planetSolarYear) {
+		let planetSolarAge = solarAge(planetSolarYear);
+		this.galacticAges.push(planetSolarAge);
+		return this.galacticAges;			
 		});
 	}
 };
