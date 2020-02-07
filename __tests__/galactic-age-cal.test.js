712e2eb07541eb823703cff8febbda1eaec69b78
Student <example@example.com>
2020-02-07 10:57:22 -0800

    test for solar age calculation fails. method not defined

--- __tests__/galactic-age-cal.test.js
+++ __tests__/galactic-age-cal.test.js
@@ -1,8 +1,20 @@
 import { GalacticAge } from './../src/galactic-age-calc.js';
 
 describe ( 'GalacticAge', () => {
+
+	// beforeEach(()=> {
+	// 	let galacticAge = new GalacticAge(31);
+	// });
+
 	test('should create a GalacticAge object with an age property', () => {
-		var galacticAge = new GalacticAge(31);
+		let galacticAge = new GalacticAge(31);
 		expect(galacticAge.age).toEqual(31);
 	});
+
+
+	test('should develop method to calculate user solar age given days in planet solar year', () => {
+		let galacticAge = new GalacticAge(31);
+		expect(galacticAge.solarAge()).toEqual(128.57);
+	});
+
 }); 
--- src/galactic-age-calc.js
+++ src/galactic-age-calc.js
@@ -2,4 +2,8 @@ export class GalacticAge {
 	constructor(age) {
 		this.age = age;
 	}
+	
+
+
 };
+
