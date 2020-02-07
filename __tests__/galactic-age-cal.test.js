1c6840be04f5aa7ca3ccf99aaddc76eaa740932e
Student <example@example.com>
2020-02-07 11:52:26 -0800

    refactor testing code

--- __tests__/galactic-age-cal.test.js
+++ __tests__/galactic-age-cal.test.js
@@ -1,20 +1,25 @@
 import { GalacticAge } from './../src/galactic-age-calc.js';
 
 describe ( 'GalacticAge', () => {
+	
+	let galacticAge;
 
-	// beforeEach(()=> {
-	// 	let galacticAge = new GalacticAge(31);
-	// });
+	beforeEach(()=> {
+		galacticAge = new GalacticAge(31);
+	});
 
 	test('should create a GalacticAge object with an age property', () => {
-		let galacticAge = new GalacticAge(31);
 		expect(galacticAge.age).toEqual(31);
 	});
 
 
 	test('should develop method to calculate user solar age given days in planet solar year', () => {
-		let galacticAge = new GalacticAge(31);
-		expect(galacticAge.solarAge(88)).toEqual(128.58);
+		// let galacticAge = new GalacticAge(31);
+		expect(galacticAge.solarAge(87.6)).toEqual(129.17);
 	});
 
+	// test('should add an array to solarAge method that includes each planets solar years', () =>	{
+	// 	let galacticAge = new GalacticAge(31);
+	// 	expect(galacticAge.solarAge).toEqual([87.6, 224.548, 365, 686.5285, 4329.557, 10759, 30688.5, 60182])
+	// });
 }); 
