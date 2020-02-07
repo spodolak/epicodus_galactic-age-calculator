0df3a6b6f0bf2e87e92ad84989532a9183a155d3
Student <example@example.com>
2020-02-07 09:52:23 -0800

    test for GalacticGe object pass

--- __tests__/galactic-age-cal.test.js
+++ __tests__/galactic-age-cal.test.js
@@ -3,6 +3,6 @@ import { GalacticAge } from './../src/galactic-age-calc.js';
 describe ( 'GalacticAge', () => {
 	test('should create a GalacticAge object with an age property', () => {
 		var galacticAge = new GalacticAge(31);
-		expect(GalacticAge.age).toEqual(31);
+		expect(galacticAge.age).toEqual(31);
 	});
 }); 
