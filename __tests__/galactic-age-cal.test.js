1c1f9148c46862bb31f56e5e9a3a81282b185498
Student <example@example.com>
2020-02-07 16:13:57 -0800

    develop test for function which calculates life expectancy in remaining years for each planet. test fails as expected

--- __tests__/galactic-age-cal.test.js
+++ __tests__/galactic-age-cal.test.js
@@ -13,13 +13,15 @@ describe ( 'GalacticAge', () => {
 	});
 	
 	test('should calculate user solar age when passed a unique planets solar year', () => {
-		expect(galacticAge.solarAge(87.6)).toEqual(129.17);
+		expect(galacticAge.solarAgeConverter(87.6)).toEqual(129.17);
 	});
 
-	test('should run the users solar age calculation for each planet. returns an array of planet ages', () =>	{
+	test('should run the users solar age converter for each planet and returns an array of planet ages', () =>	{
 		expect(galacticAge.calculateGalacticAges()).toEqual([129.17, 50.39, 31, 16.48, 2.61, 1.05, 0.37, 0.19]);
 	});
 
-	test
+	test('should calculate and convert years left to live on each planet based on inputted age and 100 earth-year life expectancy', () => {
+		expect(galacticAge.calculateGalacticLifeExpectancy()).toEqual([287.5, 112.16, 69, 36.68, 5.87, 2.34, 0.82, 0.41]);
+	});
 }); 
 
--- src/galactic-age-calc.js
+++ src/galactic-age-calc.js
@@ -4,7 +4,7 @@ export class GalacticAge	{
 		this.galacticAges = [];
 	}
 	
-	solarAge(planetSolarYear)	{
+	solarAgeConverter(planetSolarYear)	{
 		let planetAge = +((this.age*365)/planetSolarYear).toFixed(2);
 		return planetAge;
 	}
@@ -13,7 +13,7 @@ export class GalacticAge	{
 		const planetSolarYears = [87.6, 224.548, 365, 686.5285, 4329.557, 10759, 30688.5, 60182]; //values of planet year standardized to earth days
 		let galacticAge = this;
 		planetSolarYears.forEach(function(planetSolarYear) {
-		let planetSolarAge = galacticAge.solarAge(planetSolarYear);
+		let planetSolarAge = galacticAge.solarAgeConverter(planetSolarYear);
 		galacticAge.galacticAges.push(planetSolarAge);
 	});
 	return this.galacticAges;			
